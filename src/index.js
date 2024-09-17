2638795b6dfca6afd23de13137e322ffedbfc82a
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-09-17 11:01:34 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from "express"
+import connectDB from "./db/index.js"
+import creatorRoute from './routes/creator.route.js'
+import userRoute from './routes/user.route.js'
+
+const app = express();
+
+connectDB()
+.then(()=>{
+    console.log("- - MongoDB Connected - -");
+    app.listen(process.env.PORT,()=>{
+        console.log(`- - SERVER STARTED ON PORT : ${process.env.PORT} - -`);
+    })
+}
+).catch((err)=>{
+    console.error("MongoDB connection failed :- ",err);
+})
+
+app.use("/v1/user", userRoute)
+app.use("/v1/creator", creatorRoute)
