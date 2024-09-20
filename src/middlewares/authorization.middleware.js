1c9cb7f389f08c1e2ef067056811e7c7ae86bc89
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-09-20 10:30:47 +0530

    add db & middleware

---
+++ src/middlewares/authorization.middleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+
+function authMiddlewareCreator(req, res, next){
+    const authHeader = req.header("authorization");
+    try {
+        const decoded = jwt.verify(authHeader,process.env.JWT_SECRET_CREATOR);
+        if(decoded.creatorId){
+            req.creatorId = decoded.creatorId;            
+            return next();
+        } else {
+            return res.status(401).json({mesasage: "no admin found ! !"});
+        }
+    } catch (error) {
+        return res.status(401).json({mesasage: "no admin found ! !"});
+    }
+}
+function authMiddlewareUser(req, res, next){
+    const authHeader = req.headers["authorization"];
+    try {
+        const decodedCreator = jwt.verify(authHeader, process.env.JWT_SECRET_USER);
+        if(decodedCreator.userId){
+            req.userId = decodedCreator.userId;
+            return next();
+        } else {
+            return res.status(401).json({mesasage: "no user found ! "});
+        }
+    } catch (error) {
+        return res.status(401).json({mesasage: "no user found ! !"});
+    }
+}
+
+export {
+    authMiddlewareAdmin,
+    authMiddlewareUser
+}
