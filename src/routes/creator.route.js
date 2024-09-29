37f8c8ad055c40b1815a2db4875ecbe64dcfc461
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-09-29 19:11:52 +0530

    add signup creator-user

--- src/routes/creator.route.js
+++ src/routes/creator.route.js
@@ -5,7 +5,7 @@ import {generateIndexPage} from "../utils/gemini.js"
 const router = express.Router();
 router
     .post('/login', async(req, res)=>{
-        const {name, email, password} = req.body;
+        const {email, password} = req.body;
         const existedCreator = await Creator.findOne({
             $or: [{ email }]
         })
@@ -18,6 +18,18 @@ router
                 creatorId: existedCreator.id,
             }, process.env.JWT_SECRET_CREATOR)
             res.status(201).json({token});
+        } else {
+            res.send("Mail id is not associated with an account. Create a account first ! !");
+        }
+    })
+router
+    .post('/signup', async(req, res)=>{
+        const {name, email, password} = req.body;
+        const existedCreator = await Creator.findOne({
+            $or: [{ email }]
+        })
+        if(existedCreator){
+            res.send("Mail id is already associated with an account. Use a different one ! !");
         } else {
             const creator = await Admin.create({
                 name,
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.model.js"
 const router = express.Router();
 router
     .post('/login', async(req, res)=>{
-        const {name, email, password, educationLevel, interests, duration, priorKnowledge, futureCareerInterest } = req.body;
+        const {email, password} = req.body;
         const existedUser = await User.findOne({
             $or: [{ email }]
         })
@@ -17,6 +17,18 @@ router
                 userId: existedUser.id,
             }, process.env.JWT_SECRET_USER)
             res.status(201).json({token});
+        } else {
+            res.send("Mail id is not associated with an account. Create a account first ! !");
+        }
+    })
+router
+    .post('/signup', async(req, res)=>{
+        const {name, email, password} = req.body;
+        const existedUser = await User.findOne({
+            $or: [{ email }]
+        })
+        if(existedUser){
+            res.send("Mail id is already associated with an account. Use a different one ! !");
         } else {
             const user = await User.create({
                 name,
