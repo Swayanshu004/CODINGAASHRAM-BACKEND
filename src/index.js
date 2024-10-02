926f35c6c558e4b61a0d0a4fab228a6d73d05de4
Swayanshu Satyapragyan Sahoo <125992313+Swayanshu004@users.noreply.github.com>
2024-10-02 23:10:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import cors from 'cors'
 const app = express();
 
 const corsOptions = {
-    origin: 'https://www.codingaashram.com',
+    origin: 'https://codingaashram-frontend.vercel.app',
     methods: "GET, POST, PATCH, DELETE",
     credential: true
   }
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.codingaashram.com'); 
+  res.header('Access-Control-Allow-Origin', 'https://codingaashram-frontend.vercel.app'); 
   res.header('Access-Control-Allow-Credentials', 'true'); 
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
