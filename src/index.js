bee3d0c5d83466b110b6d864741320c6a46cf89a
Swayanshu Satyapragyan Sahoo <125992313+Swayanshu004@users.noreply.github.com>
2024-10-02 22:39:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const corsOptions = {
   }
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://promotionlink.vercel.app'); 
+  res.header('Access-Control-Allow-Origin', 'https://www.codingaashram.com'); 
   res.header('Access-Control-Allow-Credentials', 'true'); 
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
